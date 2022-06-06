cd0a313d0c44f8147153dbda8a38ae3140ce22ff
David <davidsiqueira.hog@gmail.com>
2022-06-06 10:24:11 -0300

    comentários

--- principal.py
+++ principal.py
@@ -12,6 +12,7 @@ def read_conf():
     config = ConfigParser()
     config.read('autonews/config.ini')
     
+    # le as configurações no arquivo config.ini
     for key in config['default']:
         conf_dict[key] = config['default'][key]
 
@@ -19,11 +20,13 @@ def read_conf():
  
 
 def get_news(conf):
+
+    # log básico para debug do request caso necessário
     logging.basicConfig(filename="app.log", level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.info("Sending HTTP GET")
    
-
+    # parâmetros do request
     query_params = {
         'apiKey': conf['apikey'],
         'country': 'br',
@@ -32,6 +35,8 @@ def get_news(conf):
         'pageSize': int(conf['numerodenoticias'])
 
     }
+
+    # faz o request
     response = requests.get('https://newsapi.org/v2/top-headlines', params=query_params)
     if response.status_code == 200:
         return response.json()
@@ -41,6 +46,8 @@ def get_news(conf):
 
 
 def write_news(json):
+
+    # escreve o arquivo de despejo das noticias.
     with open('autonews/news.txt', 'w') as f:
         f.write(str(json))
 
